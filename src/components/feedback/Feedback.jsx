f0e3e56d412fb2875de0bf6d43fa86be9499d629
Salarisdan <salarisdan@gmail.com>
2023-02-26 19:22:54 +0200

    comit

---
+++ src/components/feedback/Feedback.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Section from './modul/Section';
+import FeedbackOptions from './modul/FeedbackOptions';
+import Statistics from './modul/Statistics';
+import Notification from './modul/Notification';
+import css from './feedback.module.css';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  
+  onLeaveFeedback = label => {
+    this.setState(prevState => ({
+      [label]: prevState[label] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return (
+      this.countTotalFeedback() &&
+      Math.round((this.state.good / this.countTotalFeedback()) * 100)
+    );
+  };
+  render() {
+    return (
+      <div className={css.feedback__all}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        {this.countTotalFeedback() ? (
+          <Section title="Statistics">
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Feedback;
---
+++ src/components/feedback/modul/FeedbackOptions.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import css from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(option => (
+      <button
+        onClick={() => onLeaveFeedback(option)}
+        type="button"
+        key={option}
+        className={css.button}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+export default FeedbackOptions;
